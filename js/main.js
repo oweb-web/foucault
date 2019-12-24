6c12ae997bbdf17771cf8adc08e688a3b7a25e95
oweb-web <dhekwjd26@naver.com>
2019-12-24 12:29:38 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -68,15 +68,13 @@ $(function(){
   // BX슬라이더
   $('.bx-slider').bxSlider({
     // 여기에 옵션 설정
-    mode: 'horizontal',      // 효과설정
-    auto: true,              // 자동재생
-    easing: "ease-in-out",   // 가속도(timing-function)
-    pause: 4000,             // 정지시간(대기)
-    controls: false,         // prev, next 표시/비표시
-    pager: true,             // 하단 인디케이터 표시/비표시
-    responsive: false,       // 끄지 않으면 가변으로 움직인다(지금 베너는 고정 시켜뒀기 때문에 끔.)
-    autoHover: true
-  
+    mode: 'horizontal',     // 효과설정
+    auto: false,             // 자동재생
+    easing: "ease-in-out",  // 가속도(timing-function)
+    pause: 4000,            // 정지시간(대기)
+    controls: false,        // prev, next 표시/비표시
+    pager: true,           // 하단 인디케이터 표시/비표시
+    responsive: false,     // 끄지 않으면 가변으로 움직인다(지금 베너는 고정 시켜뒀기 때문에 끔.)
   });
 
   $('.brand-slider').bxSlider({
@@ -88,5 +86,4 @@ $(function(){
     moveSlides: 1,
   }); 
 
-  
 });
